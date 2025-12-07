7ae858ba8f736782234f57bd322564eac5ca9216
eggc <no.eggchicken@gmail.com>
2025-12-07 22:34:04 +0900

    ✨ feat: ノート切り替えをわかりやすく

--- app/src/main/java/net/eggc/ryoikumemo/MainActivity.kt
+++ app/src/main/java/net/eggc/ryoikumemo/MainActivity.kt
@@ -7,12 +7,14 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccessTime
+import androidx.compose.material.icons.filled.ArrowDropDown
 import androidx.compose.material.icons.filled.Book
 import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material.icons.filled.Timeline
@@ -30,6 +32,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.vector.ImageVector
@@ -121,10 +124,16 @@ fun RyoikumemoApp() {
             topBar = {
                 TopAppBar(
                     title = {
-                        Text(
-                            currentNote?.name ?: "",
-                            modifier = Modifier.clickable { currentDestination = AppDestinations.NOTE }
-                        )
+                        Row(
+                            modifier = Modifier.clickable { currentDestination = AppDestinations.NOTE },
+                            verticalAlignment = Alignment.CenterVertically
+                        ) {
+                            Text(currentNote?.name ?: "")
+                            Icon(
+                                Icons.Default.ArrowDropDown,
+                                contentDescription = "ノートを切り替え"
+                            )
+                        }
                     },
                     actions = {
                         if (currentUser != null) {
