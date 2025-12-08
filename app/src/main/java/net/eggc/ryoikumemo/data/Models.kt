afb815c3c82705972a0177076c9bd34682079088
eggc <no.eggchicken@gmail.com>
2025-12-08 23:30:14 +0900

    ✨ feat: たのしいアイコンの視認性を良くする

--- app/src/main/java/net/eggc/ryoikumemo/data/Models.kt
+++ app/src/main/java/net/eggc/ryoikumemo/data/Models.kt
@@ -5,7 +5,7 @@ import androidx.compose.material.icons.filled.Bedtime
 import androidx.compose.material.icons.filled.Hexagon
 import androidx.compose.material.icons.filled.LocalFireDepartment
 import androidx.compose.material.icons.filled.Medication
-import androidx.compose.material.icons.filled.SentimentSatisfied
+import androidx.compose.material.icons.filled.SentimentSatisfiedAlt
 import androidx.compose.material.icons.filled.SentimentVeryDissatisfied
 import androidx.compose.material.icons.filled.StickyNote2
 import androidx.compose.material.icons.filled.WaterDrop
@@ -31,7 +31,7 @@ enum class StampType(val label: String, val icon: ImageVector) {
     SLEEP("ねる", Icons.Default.Bedtime),
     WAKE_UP("おきる", Icons.Default.WbSunny),
     PAINFUL("つらい", Icons.Default.SentimentVeryDissatisfied),
-    FUN("たのしい", Icons.Default.SentimentSatisfied),
+    FUN("たのしい", Icons.Filled.SentimentSatisfiedAlt),
     TANTRUM("かんしゃく", Icons.Default.LocalFireDepartment),
     MEDICATION("おくすり", Icons.Default.Medication),
     POO("うんち", Icons.Filled.Hexagon),
